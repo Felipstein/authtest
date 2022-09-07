a66fa25bc08d65f23aa821ecbca25ca2459ee4a6
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-09-07 13:25:50 -0300

    feat: home main started

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,14 @@
 import Header from '../../components/Header';
 
+import { Container } from './styles';
+
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Container>
+        aa
+      </Container>
+    </>
   );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  position: relative;
+  top: 132px;
+
+  width: 90vw;
+  max-width: 1360px;
+  margin: auto;
+
+  background-color: red;
+
+`;
