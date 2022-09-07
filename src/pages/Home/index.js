6047f08259f0c967abeaebbf6dc73840f61d3cfe
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-09-07 13:56:39 -0300

    feat: refactored aside buttons

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import Header from '../../components/Header';
 import AsideButton from '../../components/AsideButton';
 
@@ -7,20 +8,28 @@ import perfilIcon from '../../assets/images/icons/perfil.svg';
 import { Container, AsideStyled } from './styles';
 
 export default function Home() {
+  const menus = useMemo(() => [
+    {
+      icon: [accountDataIcon, 'Account Data Icon'],
+      text: 'Dados da conta',
+    },
+    {
+      icon: [perfilIcon, 'Perfil Icon'],
+      text: 'Meu Perfil',
+    },
+  ]);
+
   return (
     <>
       <Header />
       <Container>
         <AsideStyled>
-          <AsideButton>
-            <img src={accountDataIcon} alt="Account Data Icon" />
-            <span>Dados da conta</span>
-          </AsideButton>
-
-          <AsideButton>
-            <img src={perfilIcon} alt="Perfil Icon" />
-            <span>Meu perfil</span>
-          </AsideButton>
+          {menus.map(({ icon, text }) => (
+            <AsideButton key={Math.random()}>
+              <img src={icon[0]} alt={icon[1]} />
+              <span>{text}</span>
+            </AsideButton>
+          ))}
         </AsideStyled>
       </Container>
     </>
