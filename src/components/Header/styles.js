23b98545e8af73ff3edf47242c6d7e0d38969cf1
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-09-07 13:22:54 -0300

    feat: header finished

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,6 +1,23 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  max-width: 1360px;
+
+  padding: 30px 40px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: rgba(255, 255, 255, 0.4);
+  -webkit-backdrop-filter: blur(4px);
+  backdrop-filter: blur(4px);
 
   img {
     width: 224px;
@@ -8,10 +25,15 @@ export const Container = styled.header`
   }
 
   .profile-menu {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
     h1 {
-      font-size: 24px;
+      font-size: 20px;
       font-weight: 700;
       text-transform: uppercase;
+      margin-right: 20px;
     }
 
     img {
