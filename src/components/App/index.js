5b3ff958f9763600a85d1f1bffe12276579a7202
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-09-07 12:55:43 -0300

    feat: default theme provider

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,13 @@
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../../assets/theme/global';
+
+import defaultTheme from '../../assets/theme/default';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <h1>App</h1>
-    </>
+    </ThemeProvider>
   );
 }
